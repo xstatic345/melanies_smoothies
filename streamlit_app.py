3e13007b4f06ce8d887b791d40f212b383bf7721
xstatic345 <131473090+xstatic345@users.noreply.github.com>
2025-11-11 09:32:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +9,8 @@ st.write(
   """
 )
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnz.sexxion()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
