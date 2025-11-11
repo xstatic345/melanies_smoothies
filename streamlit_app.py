2a9681440a99ae42c8027735fd30afb9d6d665d5
xstatic345 <131473090+xstatic345@users.noreply.github.com>
2025-11-11 10:11:32 +0100

    Fix search value display for selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if ingredients_list:
         ingredients_string += fruit + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit,' is ', search_on, '.')
       
         st.subheader(fruit + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
