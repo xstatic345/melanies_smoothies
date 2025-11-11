224b14b71c52477c4ece8d41a792d242579c90b5
xstatic345 <131473090+xstatic345@users.noreply.github.com>
2025-11-11 10:06:55 +0100

    Use Pandas DataFrame for displaying fruit options
    Convert Snowflake DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Example Streamlit App :cup_with_straw:")
@@ -13,9 +14,13 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
-                                                                      
+
 name_on_order = st.text_input('Name on smoothie:')
 if name_on_order:
     st.write('The name on your smoothie will be: ',name_on_order)
