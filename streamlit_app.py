2b184666a6787bfbeab2c5ed500e0dab1ef17000
xstatic345 <131473090+xstatic345@users.noreply.github.com>
2025-11-11 09:47:13 +0100

    Fix typo in session initialization

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
 )
 
 cnx = st.connection("snowflake")
-session = cnz.sexxion()
+session = cnx.sexxion()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
